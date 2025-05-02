043993eaf0428ceade93e12854995a8740befac1
deepgeneral33@gmail.com <deepgeneral33@gmail.com>
2025-05-02 16:02:40 +0545

    Admin complaints page ready

--- admin/admin_complaints.php
+++ admin/admin_complaints.php
@@ -1,13 +1,47 @@
 <?php
 include '../includes/db.php';
 include '../admin/admin_navbar.php';
-$query = "SELECT complaints.*, products.name AS product_name 
-          FROM complaints JOIN products ON complaints.product_id = products.id";
-$result = mysqli_query($conn, $query);
-echo "<h2>Complaints</h2><table border='1'><tr><th>Name</th><th>Email</th><th>Product</th><th>Message</th></tr>";
-while ($row = mysqli_fetch_assoc($result)) {
-    echo "<tr><td>{$row['user_name']}</td><td>{$row['user_email']}</td>
-          <td>{$row['product_name']}</td><td>{$row['message']}</td></tr>";
-}
-echo "</table>";
-include '../includes/footer.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Complaints</title>
+    <link rel="stylesheet" href="/OCS/css/style.css">
+</head>
+
+<body>
+
+    <div class="complaints-container">
+        <h2>User <span style="color:#f85903">Complaints</span></h2>
+        <p style="color: #003266;">Here is the list of all the complaints submitted by the users.</p>
+        <table>
+            <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Product</th>
+                <th>Message</th>
+            </tr>
+            <?php
+            $query = "SELECT complaints.*, products.name AS product_name 
+        FROM complaints JOIN products ON complaints.product_id = products.id";
+            $result = mysqli_query($conn, $query);
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo "<tr>
+            <td>{$row['user_name']}</td>
+            <td>{$row['user_email']}</td>
+            <td>{$row['product_name']}</td>
+            <td>{$row['message']}</td>
+            </tr>";
+            }
+            ?>
+        </table>
+    </div>
+
+    <?php include '../includes/footer.php'; ?>
+
+</body>
+
+</html>
